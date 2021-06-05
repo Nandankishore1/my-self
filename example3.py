fb8c30883523606b8b4f7d01aea8c79ca8a33294
nandan kishore <munganandankishore@gmail.com>
2021-06-05 09:52:16 +0530

    about my self

---
+++ example3.py
@@ -0,0 +1,12 @@
+my_name = 'Nandan Kishore'
+my_age = 20
+my_height = 171
+my_weight = 62
+my_eyes = 'black'
+my_hair = 'black'
+
+print(f"let's talk about {my_name}")
+print(f"he's {my_height} cm tall")
+print(f"he's {my_weight} kgs heavy")
+print(f"he's got {my_eyes} eyes and {my_hair} hair")
+
